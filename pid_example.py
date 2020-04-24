320f2b0b77c462642fd03f99338d069bcd8fbfb0
Ahmet Ataşoğlu <ahmetatasoglu98@gmail.com>
2020-04-24 13:11:03 +0300

    Add files via upload

---
+++ pid_example.py
@@ -0,0 +1,41 @@
+import math
+import matplotlib.pyplot as plt
+from pidControl import pidControl
+
+# parametreler
+ref = 50
+Kpid = [0.25, 0.001, 0.0001]
+dt = 1
+h0 = 0
+step = 30
+
+# sistem modeli
+sys_model = lambda x: 0.1*math.pow(x, 2)
+
+"""
+# sistem modeli şu şekilde de tanımlanabilirdi:
+def f(x):
+    return 0.1*math.pow(x, 2)
+sys_model = f
+"""
+
+ctrl = pidControl(ref, dt, Kpid)
+ctrl.set_sys_model(sys_model)
+
+output = [h0]
+for i in range(step):
+    last_fb = output[-1]
+    x = ctrl.one_shot(last_fb)
+    output.append(x + last_fb)
+
+# print(output)
+
+
+# görselleştirme
+
+plt.axhline(ref, color = 'r', label = 'referans-degeri')
+plt.plot(range(step+1), output, 'b', marker='o', label = 'irtifa')
+plt.xlabel('zaman')
+plt.ylabel('cikis')
+plt.legend()
+plt.show()
